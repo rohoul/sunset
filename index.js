96c589ebc6cdf1b5b0299f32e63086c1c172c6d9
Oul Roh <oulroh@Oului-iMac.local>
2020-11-09 17:53:44 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -2,14 +2,41 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const config = require('./config/key');
+
+const bodyParser = require('body-parser');
+const {User} = require("./models/User");
+
+// bodyParser 옵션
+// body-parser : 클라이언트에서 오는 정보를 서버에서 분석해서 가져올 수 있게 해주는 것
+
+// 1) application/x-www-form-urlencoded 분석해서 가져오는 것
+app.use(bodyParser.urlencoded({extended: true}));
+// 2) application/json 타입으로 된 것을 분석해서 가져오는 것
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://test_user:dkakektm90@sunset.eney5.mongodb.net/<dbname>?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser : true, useUnifiedTopology : true, useCreateIndex: true, useFindAndModify:false
 }).then(()=> console.log('MongoDB Connected..!'))
     .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 안녕하세요~')
+  res.send('Hello World! 안녕하세요~ 얄루얄루')
+})
+
+// 클라이언트에서 보내주는 정보들(회원가입을 위한 라우트)
+// 회원가입 할때 필요한 정보들을 client에서 가져오면 데이터베이스에 넣어준다.
+app.post('/register', (req, res) => {
+    // body-parser가 request body에 넣어준다.
+    const user = new User(req.body)
+    // save : mongoDB에서 오는 메소드
+    user.save((err,userInfo) => {
+        if(err) return res.json({success:false, err})
+        return res.status(200).json({
+            success:true
+        })
+    })
 })
 
 app.listen(port, () => {
